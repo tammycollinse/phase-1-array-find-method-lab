1be8ed07e5a43d9f635b6928583a667f46dff2f4
Tammy <tammycollinse@gmail.com>
2022-02-26 15:25:01 -0500

    commit tree d39793469509d58cbf942523a0f78582c557a230

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+// the objective tonfind if the team had ever won a SuperBowl
+//we have an array called record with objects with year:result key:value pairs
+
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+function superbowlWin(recordArr){
+ for (const item of recordArr){
+     if (item.result === "W"){
+      console.log(item.year);
+      return item.year;
+     }
+ }
+}
+
+record.find(superbowlWin(record));
